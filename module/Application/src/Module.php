a724a51de297bc52d0d3be93d9efb443409bd3e5
christrodr <christianrodrigue@gmail.com>
2018-11-28 18:36:14 +0100

    factories for SectionController

--- module/Application/src/Module.php
+++ module/Application/src/Module.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * @link      http://github.com/zendframework/ZendSkeletonApplication for the canonical source repository
  * @copyright Copyright (c) 2005-2016 Zend Technologies USA Inc. (http://www.zend.com)
@@ -7,12 +8,34 @@
 
 namespace Application;
 
-class Module
-{
+use Zend\Db\Adapter\AdapterInterface;
+use Zend\Db\ResultSet\ResultSet;
+use Zend\Db\TableGateway\TableGateway;
+use Zend\ModuleManager\Feature\ConfigProviderInterface;
+
+class Module implements ConfigProviderInterface {
+
     const VERSION = '3.0.3-dev';
 
-    public function getConfig()
-    {
-        return include __DIR__ . '/../config/module.config.php';
+    public function getConfig() {
+	return include __DIR__ . '/../config/module.config.php';
     }
+
+    public function getServiceConfig() {
+	return [
+	    'factories' => [
+		Model\SectionTable::class => function($container) {
+		    $tableGateway = $container->get(Model\SectionTableGateway::class);
+		    return new Model\SectionTable($tableGateway);
+		},
+		Model\SectionTableGateway::class => function ($container) {
+		    $dbAdapter = $container->get(AdapterInterface::class);
+		    $resultSetPrototype = new ResultSet();
+		    $resultSetPrototype->setArrayObjectPrototype(new Model\Section());
+		    return new TableGateway('section', $dbAdapter, null, $resultSetPrototype);
+		},
+	    ],
+	];
+    }
+
 }
