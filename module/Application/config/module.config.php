33450b9d9ec31152aecc99b87b37bb5b4821d78d
christrodr <christianrodrigue@gmail.com>
2018-11-30 16:00:02 +0100

    flashmessenger helper

--- module/Application/config/module.config.php
+++ module/Application/config/module.config.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * @link      http://github.com/zendframework/ZendSkeletonApplication for the canonical source repository
  * @copyright Copyright (c) 2005-2016 Zend Technologies USA Inc. (http://www.zend.com)
@@ -9,46 +10,47 @@ namespace Application;
 
 use Zend\Router\Http\Literal;
 use Zend\Router\Http\Segment;
+
 //use Zend\ServiceManager\Factory\InvokableFactory;
 
 return [
     'router' => [
-        'routes' => [
-            'home' => [
-                'type' => Literal::class,
-                'options' => [
-                    'route'    => '/',
-                    'defaults' => [
-                        'controller' => Controller\IndexController::class,
-                        'action'     => 'index',
-                    ],
-                ],
-            ],
-            'application' => [
-                'type'    => Segment::class,
-                'options' => [
-                    'route'    => '/application[/:action]',
-                    'defaults' => [
-                        'controller' => Controller\IndexController::class,
-                        'action'     => 'index',
-                    ],
-                ],
-            ],
+	'routes' => [
+	    'home' => [
+		'type' => Literal::class,
+		'options' => [
+		    'route' => '/',
+		    'defaults' => [
+			'controller' => Controller\IndexController::class,
+			'action' => 'index',
+		    ],
+		],
+	    ],
+	    'application' => [
+		'type' => Segment::class,
+		'options' => [
+		    'route' => '/application[/:action]',
+		    'defaults' => [
+			'controller' => Controller\IndexController::class,
+			'action' => 'index',
+		    ],
+		],
+	    ],
 	    'section' => [
-                'type'    => Segment::class,
-                'options' => [
-                    'route' => '/section[/:action[/:id]]',
-                    'constraints' => [
-                        'action' => '[a-zA-Z][a-zA-Z0-9_-]*',
-                        'id'     => '[0-9]+',
-                    ],
-                    'defaults' => [
-                        'controller' => Controller\SectionController::class,
-                        'action'     => 'index',
-                    ],
-                ],
-            ],
-        ],
+		'type' => Segment::class,
+		'options' => [
+		    'route' => '/section[/:action[/:id]]',
+		    'constraints' => [
+			'action' => '[a-zA-Z][a-zA-Z0-9_-]*',
+			'id' => '[0-9]+',
+		    ],
+		    'defaults' => [
+			'controller' => Controller\SectionController::class,
+			'action' => 'index',
+		    ],
+		],
+	    ],
+	],
     ],
 //    'controllers' => [
 //        'factories' => [
@@ -57,19 +59,26 @@ return [
 //        ],
 //    ],
     'view_manager' => [
-        'display_not_found_reason' => true,
-        'display_exceptions'       => true,
-        'doctype'                  => 'HTML5',
-        'not_found_template'       => 'error/404',
-        'exception_template'       => 'error/index',
-        'template_map' => [
-            'layout/layout'           => __DIR__ . '/../view/layout/layout.phtml',
-            'application/index/index' => __DIR__ . '/../view/application/index/index.phtml',
-            'error/404'               => __DIR__ . '/../view/error/404.phtml',
-            'error/index'             => __DIR__ . '/../view/error/index.phtml',
-        ],
-        'template_path_stack' => [
-            __DIR__ . '/../view',
-        ],
+	'display_not_found_reason' => true,
+	'display_exceptions' => true,
+	'doctype' => 'HTML5',
+	'not_found_template' => 'error/404',
+	'exception_template' => 'error/index',
+	'template_map' => [
+	    'layout/layout' => __DIR__ . '/../view/layout/layout.phtml',
+	    'application/index/index' => __DIR__ . '/../view/application/index/index.phtml',
+	    'error/404' => __DIR__ . '/../view/error/404.phtml',
+	    'error/index' => __DIR__ . '/../view/error/index.phtml',
+	],
+	'template_path_stack' => [
+	    __DIR__ . '/../view',
+	],
+    ],
+    'view_helper_config' => [
+	'flashmessenger' => [
+	    'message_open_format' => '<div%s><button type="button" class="close" data-dismiss="alert" aria-hidden="true">&times;</button><ul><li>',
+	    'message_close_string' => '</li></ul></div>',
+	    'message_separator_string' => '</li><li>',
+	],
     ],
 ];
