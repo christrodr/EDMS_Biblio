251b71498334529bf07701da6252c0aaf8fb51e6
christrodr <christianrodrigue@gmail.com>
2018-11-30 09:47:15 +0100

    custom.js datatable for section index

--- public/js/custom.js
+++ public/js/custom.js
@@ -3,5 +3,11 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-
+$(document).ready(function () {
+    $('#sectionsList').DataTable({
+        "oLanguage": {
+            "sUrl": "/js/plugins/dataTable/fr_FR.txt"
+        }
+    });
+});
 
