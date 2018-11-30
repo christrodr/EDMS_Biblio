12431ecda4373aee28028043f23656347d671ba1
christrodr <christianrodrigue@gmail.com>
2018-11-30 14:03:49 +0100

    Ajout des validators du sectionForm

--- module/Application/src/Model/Section.php
+++ module/Application/src/Model/Section.php
@@ -8,16 +8,70 @@
 
 namespace Application\Model;
 
-class Section {
+use DomainException;
+use Zend\Filter\StringTrim;
+use Zend\Filter\StripTags;
+use Zend\Filter\ToInt;
+use Zend\InputFilter\InputFilter;
+use Zend\InputFilter\InputFilterAwareInterface;
+use Zend\InputFilter\InputFilterInterface;
+use Zend\Validator\StringLength;
+
+class Section implements InputFilterAwareInterface {
 
     public $id;
     public $nom;
     public $archiver;
+    private $inputFilter;
 
     public function exchangeArray(array $data) {
 	$this->id = !empty($data['id_Section']) ? $data['id_Section'] : null;
 	$this->nom = !empty($data['nom']) ? $data['nom'] : null;
 	$this->archiver = !empty($data['archiver']) ? $data['archiver'] : null;
+    }
+
+    public function setInputFilter(InputFilterInterface $inputFilter) {
+	throw new DomainException(sprintf(
+		'%s does not allow injection of an alternate input filter', __CLASS__
+	));
+    }
+
+    public function getInputFilter() {
+	if ($this->inputFilter) {
+	    return $this->inputFilter;
+	}
+
+	$inputFilter = new InputFilter();
+
+	$inputFilter->add([
+	    'name' => 'id',
+	    'required' => true,
+	    'filters' => [
+		['name' => ToInt::class],
+	    ],
+	]);
+
+	$inputFilter->add([
+	    'name' => 'nom',
+	    'required' => true,
+	    'filters' => [
+		['name' => StripTags::class],
+		['name' => StringTrim::class],
+	    ],
+	    'validators' => [
+		[
+		    'name' => StringLength::class,
+		    'options' => [
+			'encoding' => 'UTF-8',
+			'min' => 1,
+			'max' => 100,
+		    ],
+		],
+	    ],
+	]);
+
+	$this->inputFilter = $inputFilter;
+	return $this->inputFilter;
     }
 
 }
